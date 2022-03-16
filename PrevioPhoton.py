8996d9c679b84514150dea5c502445ba11e75d87
Adriana Herrera Godina <91992994+adrianaSung@users.noreply.github.com>
2022-03-16 12:28:56 -0600

    Add files via upload
    Previo Photon

---
+++ PrevioPhoton.py
@@ -0,0 +1,15 @@
+#HERRERA GODINA ADRIANA JOCELYN
+#PREVIO PHOTON
+def numeros(1, 3):
+    numeros = []
+    for numero1 in l:
+        for numero2 in 3:
+            if numero1 == numero2 and numero1 not in intersection:
+                numeros.append(numero1) #Agregar nuevos elementos a una lista
+    
+    print(numeros)
+
+lista1 = [4,8,16,24,32]
+lista2 = [1,2,3,4,5,6]
+
+numeros(lista1,lista2)
